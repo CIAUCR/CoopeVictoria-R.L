97d115598b6a7bd5803066cd8055e45a3f7cd541
CIAUCR <92112557+CIAUCR@users.noreply.github.com>
2022-08-10 18:19:48 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -234,23 +234,31 @@ $.getJSON("rendimientohistorico.geojson", function(geodata) {
 			return {'color': "orange", 'weight': 1, 'fillOpacity': 0.0}
 		},
 		onEachFeature: function (feature, layer) {
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
-			    </script>
-			<canvas id="myChart" style="width:10%;max-width:700px"></canvas>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+			<body>
+			    <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+			<script>
 			var yValues = [feature.properties.PROD_16,feature.properties.PROD_17];
 			var xValues = [2016,2017];
-			var myChart = new Chart("myChart", {
-				type: "scatter",
-				data: {
-					labels: xValues,
-					datasets: [{
-						backgroundColor: "rgba(0,0,0,1.0)",
-						borderColor: "rgba(0,0,0,0.1)",
-						data: yValues
-					}],
-					}
-				options: {}
-			});
+			new Chart("myChart", { type: "line",
+					      data: {
+						      labels: xValues,
+						      datasets: [{
+							      fill: false,
+							      lineTension: 0,
+							      backgroundColor: "rgba(0,0,255,1.0)",
+							      borderColor: "rgba(0,0,255,0.1)",
+							      data: yValues
+						      }]
+					      },
+					      options: {
+						      legend: {display: false},
+						      scales: {
+							      yAxes: [{ticks: {min: 6, max:16}}],
+						      }
+					      }
+					     });
+			</script>
                 }
             }).addTo(map);
 	control_layers.addOverlay(layer_geojson_historial, 'Historial  Cosecha por Finca');
