0a23c4f120653dca50031f1f74c959e442152f2f
CIAUCR <92112557+CIAUCR@users.noreply.github.com>
2022-08-11 17:14:03 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -242,6 +242,29 @@ $.getJSON("rendimientohistorico.geojson", function(geodata) {
           if (feature.properties.name === 'test1') {
             var svg = d3.select(div).select("svg").attr("width", 200).attr("height", 200);
             svg.append("rect").attr("width", 150).attr("height", 150).style("fill", "lightBlue");
+		var xyValues = [
+			{x:50, y:7},
+			{x:60, y:8},
+			{x:70, y:8}];
+		 
+		  new Chart("myChart", {
+			  type: "scatter",
+			  data: {
+				  datasets: [{
+					  pointRadius: 4,
+					  pointBackgroundColor: "rgb(0,0,255)",
+					  data: xyValues
+				  }]
+			  },
+			  options: {
+				  legend: {display: false},
+				  scales: {
+					  xAxes: [{ticks: {min: 40, max:160}}],
+					  yAxes: [{ticks: {min: 6, max:16}}],
+				  }
+			  }
+		  });  
+            svg.append("chart").attr("width", 150).attr("height", 150).style("fill", "lightBlue");
           }
         }
       }).addTo(map);
